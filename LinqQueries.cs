dafbc200a3eff345ceedef00a35fb53ce7bfef57
Santiago Agustin Plaza <santiagoagustinplaza@gmail.com>
2023-02-20 12:54:38 -0300

    Program configuration

---
+++ LinqQueries.cs
@@ -0,0 +1,19 @@
+public class LinqQueries
+{
+    private List<Book> booksCollection = new List<Book>();
+    public LinqQueries()
+    {
+        using(StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.booksCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>
+            (json, new System.Text.Json.JsonSerializerOptions()
+            {PropertyNameCaseInsensitive = true});
+        }
+
+    }
+    public IEnumerable<Book> AllTheCollection()
+    {
+        return booksCollection;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿Console.WriteLine("Hello, World!");
+
+LinqQueries queries = new LinqQueries();
+
+PrintValues(queries.AllTheCollection());
+
+void PrintValues(IEnumerable<Book> bookList)
+{
+    Console.WriteLine("{0,-60} {1,11} {2, 15}\n", "Title", "Pages", "Published Date");
+    foreach(var item in bookList)
+    {
+        Console.WriteLine("{0,-60} {1,11} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
